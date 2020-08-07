35fdfb4ed5ed9ff6698e8e841a5fa70f7fe5f9f3
alicefang <alice.fan>
2020-08-07 15:11:30 +0800

    Initial Commit

---
+++ badJokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  badJokes
+//
+//  Created by 方芸萱 on 2020/8/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
